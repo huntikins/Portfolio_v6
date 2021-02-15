6917f712bdf3f6d2c3c1951149f8c88945cb2771
Hunter Trammell <hunterttrammell@gmail.com>
2021-02-14 19:41:04 -0600

    created basic form, testing on Netlify then wil style

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,5 +1,61 @@
-<template>  
-<Layout>
-    <h1 style="text-align:center;padding:6rem;">Coming Soon</h1>
-</Layout>
+<template>
+  <Layout>
+    <div class="page_contact">
+      <section class="page_contact-intro">
+        <h1>Contact</h1>
+        <form name="contact" class="page_contact-form--group" netlify>
+            <div class="page_contact-form--group">
+                <label for="name">Full Name</label>
+                <input type="text" name="name" id="name"/>
+            </div>
+            <div class="page_contact-form--group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email"/>
+            </div>
+            <div class="page_contact-form--group">
+                <label for="message">Message</label>
+                <textarea type="text" name="message" id="message"/>
+            </div>
+            <input type="submit" value="Submit">
+        </form>
+      </section>
+    </div>
+  </Layout>
 </template>
+
+<script>
+export default {
+    metaInfo() {
+      return {
+        title: this.$page.meta.siteName,
+        description: this.$page.meta.siteDescription,
+        keywords: 'web development, javascript, html, vue, react, front-end, back-end, full-stack, coding',
+        openGraph: {
+          title: this.$page.meta.siteName,
+          type: 'website',
+          description: this.$page.meta.siteDescription
+        },
+        meta: [
+          { name: "twitter:card", content: "summary_large_image" },
+          { name: "twitter:title", content: this.$page.meta.siteName },
+          { name: "twitter:description", content: this.$page.meta.siteDescription },
+          { name: "twitter:site", content: this.$page.meta.twitter }, 
+          { name: "twitter:image", content: this.$page.meta.coverImage },
+          { name: "twitter:creator", content: this.$page.meta.twitter },
+        ]
+      }
+  }
+}
+</script>
+
+<page-query>
+query {
+  meta: metadata {
+    siteName
+    siteUrl
+    siteDescription
+    twitter
+    coverImage
+  }
+}
+</page-query>
